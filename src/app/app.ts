5a15c2d7dd099b3080f6010a214abe1ead1b44cb
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-20 16:45:49 -0300

    feat(header): implement initial gov-header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GovHeader } from './shared/components/gov-header/gov-header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GovHeader],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/gov-header/gov-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-gov-header',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './gov-header.html',
+  styleUrl: './gov-header.scss',
+})
+export class GovHeader {
+
+}
