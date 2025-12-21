a83c430a6b77e4e49aa57485cabce47b959d3c9c
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-21 19:48:53 -0300

    feat(consulta): add filters, results table and pagination UI

---
+++ src/app/pages/consulta/consulta.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsultaComponent } from './consulta.component';
+
+describe('Consulta', () => {
+  let component: ConsultaComponent;
+  let fixture: ComponentFixture<ConsultaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConsultaComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConsultaComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
