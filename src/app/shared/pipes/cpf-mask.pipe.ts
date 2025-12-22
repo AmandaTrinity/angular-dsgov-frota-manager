139dec2ff87dbf7d2c5bd4cbd5997489e2cbd45a
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-21 23:34:22 -0300

    fix: implements CPF masking

--- src/app/shared/pipes/cpf-mask.pipe.ts
+++ src/app/shared/pipes/cpf-mask.pipe.ts
@@ -2,20 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'cpfMask',
-  standalone: true,
+  standalone: true
 })
 export class CpfMaskPipe implements PipeTransform {
-  transform(value: string | number | undefined | null): string {
-    if (!value) {
-      return '';
-    }
-    const cpf = String(value).replace(/\D/g, '');
-
-    if (cpf.length !== 11) {
-      return String(value);
-    }
-
-    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  transform(value: string | undefined): string {
+    if (!value) return '';
+    
+    const limpo = value.replace(/\D/g, '');
+    
+    return `***.${limpo.substring(3, 6)}.${limpo.substring(6, 9)}-**`;
   }
 }
-
