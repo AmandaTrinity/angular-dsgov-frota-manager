e0a31c2e6b59c3836528f4ea87e316929608e118
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-21 22:38:42 -0300

    feat: add model of driver and vehicle in the model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ConsultaComponent } from './pages/consulta/consulta.component';
+import { ConsultaDetalheComponent } from './pages/consulta/consulta-detalhe/consulta-detalhe';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,9 +14,22 @@ export const routes: Routes = [
   },
   {
     path: 'consulta',
-    component: ConsultaComponent,
     data: {
       breadcrumb: 'Consulta'
-    }
+    },
+    children: [
+      {
+        path: '',
+        component: ConsultaComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: ':id',
+        component: ConsultaDetalheComponent,
+        data: {
+          breadcrumb: 'Detalhe'
+        }
+      }
+    ]
   }
 ];
--- src/app/core/models/abastecimento.model.ts
+++ src/app/core/models/abastecimento.model.ts
@@ -5,6 +5,14 @@ export interface Abastecimento {
   uf: string;
   tipoCombustivel: string;
   valorLitro: number;
+  motorista: {
+    nome: string;
+    cpf: string;
+  };
+  veiculo: {
+    placa: string;
+    modelo: string;
+  };
   quantidadeLitros: number;
   valorTotalPago: number;
 }
