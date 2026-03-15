37f6efd61e7fb6d11c3f6853afc3ea8f0ca83608
Amanda Trinity <amandatrinity16@gmail.com>
2026-03-15 15:05:25 -0300

    feat: configure environment settings for production and development

--- src/app/core/services/fuel.service.ts
+++ src/app/core/services/fuel.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Abastecimento } from '../models/abastecimento.model';
+import { environment } from 'src/app/environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FuelService {
-  private readonly apiUrl = 'http://localhost:3000/abastecimentos';
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
